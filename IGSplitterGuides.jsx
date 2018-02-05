61a9c3a8619c6e7882e534c1794d7d938f179847
CN58090 <NortonChristopherP@johndeere.com>
2018-02-05 08:48:45 -0600

    chore: initial upload

---
+++ IGSplitterGuides.jsx
@@ -0,0 +1,20 @@
+﻿var savedRuler= app.preferences.rulerUnits;
+app.preferences.rulerUnits = Units.PIXELS;
+
+var doc = app.activeDocument;
+var w = doc.width;
+var h = doc.height;
+var ratio = w/h;
+var unitVertical = w/ratio
+var leftOverSpace = unitVertical - (w - Math.round(ratio) * unitVertical);
+
+var guides = doc.guides;
+guides.add(Direction.VERTICAL, w)
+
+for (var i = 0; i < Math.round(ratio)+1; i++) { 
+    guides.add(Direction.VERTICAL, i * unitVertical);
+}
+
+if(w % h != 0) doc.resizeCanvas (w+leftOverSpace, h, AnchorPosition.MIDDLELEFT);
+
+app.preferences.rulerUnits = savedRuler;
