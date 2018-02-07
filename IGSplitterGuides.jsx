36d135ce2b0efad31300124e74f0aac9cb1451ab
Chris Norton <cnorton@aquent.com>
2018-02-07 10:43:02 -0600

    refactor: simplify and clean up logic

--- IGSplitterGuides.jsx
+++ IGSplitterGuides.jsx
@@ -5,16 +5,13 @@ var doc = app.activeDocument;
 var w = doc.width;
 var h = doc.height;
 var ratio = w/h;
-var unitVertical = w/ratio
-var leftOverSpace = unitVertical - (w - Math.round(ratio) * unitVertical);
+var divisions = Math.ceil(ratio);
 
 var guides = doc.guides;
 guides.add(Direction.VERTICAL, w)
-
-for (var i = 0; i < Math.round(ratio)+1; i++) { 
-    guides.add(Direction.VERTICAL, i * unitVertical);
+for (var i = 0; i < divisions; i++) { 
+    guides.add(Direction.VERTICAL, i * h);
 }
 
-if(w % h != 0) doc.resizeCanvas (w+leftOverSpace, h, AnchorPosition.MIDDLELEFT);
-
+doc.resizeCanvas (h*divisions, h, AnchorPosition.MIDDLELEFT);
 app.preferences.rulerUnits = savedRuler;
